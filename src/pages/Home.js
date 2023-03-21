b0f4a6135f9a4e1ef64160d64352cb26d0bb9560
marceloagoston <marceloagoston91@gmail.com>
2023-03-21 15:54:19 -0300

    Agregar Home base

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="home">
+            <div className="name"> <font>Marcelo Agoston</font></div>
+            <div className="desc"> FullStack Developer <font>React/Django</font></div>
+        </div>
     )
 };
 
