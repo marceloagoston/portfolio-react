d69a6d75686999df9e0d87ba50967dd1fab3f315
marceloagoston <marceloagoston91@gmail.com>
2023-03-21 15:45:26 -0300

    Proyecto base, agregar header y react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Services from './pages/Services';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* Enrutamiento en react con ROUTER */}
+      <Router>
+        <Header />
+        <Routes>
+          {/* Esta es la ruta individual */}
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/services" element={<Services />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contacts" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
